3b0d18875ca99328bbef985e91d227ef225fa4a8
Moo <ervinmoses4@gmail.com>
2026-06-30 00:50:16 +0800

    update

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,22 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-database.js";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDewCnOF9r2ADMTcnxOBPQ6gR_qgOvV6sc",
   authDomain: "hightable-games.firebaseapp.com",
+  databaseURL: "https://hightable-games-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "hightable-games",
   storageBucket: "hightable-games.firebasestorage.app",
   messagingSenderId: "927858547128",
   appId: "1:927858547128:web:e6d5f153af37f318a0574d",
-  measurementId: "G-C9BY6MR05P",
-  databaseURL: "https://hightable-games-default-rtdb.firebaseio.com"
+  measurementId: "G-C9BY6MR05P"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Realtime Database and get a reference to the service
-export const db = getDatabase(app);
+const analytics = getAnalytics(app);
